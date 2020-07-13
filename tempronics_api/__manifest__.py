0d7f8ab43e30ec095eed98d390c9a4db778cf273
j-monroy <joseangel_0118@hotmail.com>
2020-07-13 16:05:00 -0700

    [ADD] Tempronics_API

---
+++ tempronics_api/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Tempronics API',
+    'version': '12.0.1.0.0',
+    'category': 'Manufacturing',
+    'author': "Jose Monroy",
+    'website': 'www.tempronics.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'mrp',
+        'product',
+        'stock'
+    ],
+    'installable': True,
+}
