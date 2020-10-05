bbf6135a5e568b1bbb0a2bf65fbd93ab998faa41
J-Monroy <jmonroy@tempronics.com>
2020-10-05 10:33:51 -0700

    Se eliminaron archivos inecesarios

--- tempronics_api/models/__init__.py
+++ tempronics_api/models/__init__.py
@@ -2,5 +2,3 @@ from . import mrp_production
 from . import product_template
 from . import trics_mrp_bom
 from . import trics_serial_group
-from . import tempronics_api_settings
-from . import res_config_settings
