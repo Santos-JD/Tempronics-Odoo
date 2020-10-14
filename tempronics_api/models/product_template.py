5ce393977e6964f14a254ea9e3c77b56f29f8844
J-Monroy <jmonroy@tempronics.com>
2020-10-13 17:02:20 -0700

    write and link funcionando

--- tempronics_api/models/product_template.py
+++ tempronics_api/models/product_template.py
@@ -30,17 +30,55 @@ class productTemplate(models.Model):
         dataEnsamble['ensamble'] = values['default_code']
         dataEnsamble['category'] = values['categ_id']
         dataEnsamble['serie'] = 0
-        if values['tracking'] == 'serial':
+        if values.get('tracking') == 'serial':
             dataEnsamble['serie'] = 1
             dataEnsamble['serial_group'] = values['serial_group']
-            
         create = super(productTemplate,self).create(values)
         dataEnsamble['id'] = create.id
         POST = requests.post(url,data = dataEnsamble)
         result = POST.json()
         if not result['success']:
             raise UserError(_("Error al crear un nuevo producto en el sistema de produccion: \ %s" % (result['msj'])))
-    
         return create
 
 
+
+    def write(self,values):
+        api = self.env['trics.config.api'].getconfigapi(self._inherit)
+        if not api.active:
+            return super(productTemplate,self).write(values)
+        
+        write = super(productTemplate,self).write(values)
+        url = api.url
+        dataEnsamble = {}
+        dataEnsamble['accion'] = 'write'
+        dataEnsamble['id'] = self.id
+        dataEnsamble['ensamble'] = self.default_code
+        dataEnsamble['descripcion'] = self.name
+        dataEnsamble['category'] = self.categ_id.id
+        dataEnsamble['active'] = self.active
+
+        if self.tracking == 'serial':
+            dataEnsamble['serie'] = 1
+            dataEnsamble['serial_group'] = self.serial_group.id
+        POST = requests.post(url,data = dataEnsamble)
+        result = POST.json()
+        if not result['success']:
+            raise UserError(_("Ocurrio un error al momento de actualiza el ensamble de produccion \n %s" % (result['msj'])))
+        return write
+
+    def unlink(self):
+        api = self.env['trics.config.api'].getconfigapi(self._inherit)
+        if not api.active:
+            return super(productTemplate,self).unlink()
+
+        url = api.url
+        dataEnsamble = {}
+        dataEnsamble['accion'] = 'unlink'
+        dataEnsamble['id'] = self.id
+
+        POST = requests.post(url,data = dataEnsamble)
+        result = POST.json()
+        if not result['success']:
+            raise UserError(_("Ocurrio un error al momento de eliminar el ensamble de produccion \n %s" % (result['msj'])))
+        return super(productTemplate,self).unlink()
