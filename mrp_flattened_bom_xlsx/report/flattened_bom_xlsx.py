ca68560c5e1942ae99eb665ae3e585db7db43142
J-Monroy <jmonroy@tempronics.com>
2021-01-04 10:36:20 -0700

    Mejorar vista

--- mrp_flattened_bom_xlsx/report/flattened_bom_xlsx.py
+++ mrp_flattened_bom_xlsx/report/flattened_bom_xlsx.py
@@ -5,6 +5,8 @@
 import logging
 from odoo import models
 from odoo.tools.translate import _
+from datetime import datetime
+import pytz
 
 _logger = logging.getLogger(__name__)
 
@@ -29,25 +31,24 @@ class FlattenedBomXlsx(models.AbstractModel):
 
     def print_flattened_bom_lines(self, bom, requirements, sheet, row, locations, cell_style):
         i = row
-        sheet.write(i, 0, bom.product_tmpl_id.name or '',cell_style)
-        sheet.write(i, 1, bom.code or '',cell_style)
-        sheet.write(i, 2, bom.display_name or '',cell_style)
-        sheet.write(i, 3, bom.product_qty,cell_style)
-        sheet.write(i, 4, bom.product_uom_id.name or '',cell_style)
-        sheet.write(i, 5, bom.product_tmpl_id.standard_price or '',cell_style)
+        sheet.write(i, 0, bom.product_tmpl_id.default_code or '',cell_style)
+        sheet.write(i, 1, bom.product_tmpl_id.name or '',cell_style)
+        sheet.write(i, 2, bom.product_qty,cell_style)
+        sheet.write(i, 3, bom.product_uom_id.name or '',cell_style)
+        sheet.write(i, 4, bom.product_tmpl_id.standard_price or '',cell_style)
         totals = self.get_totals(bom.product_tmpl_id.id,locations)
-        sheet.write_row(i,6,totals,cell_style)
+        sheet.write_row(i,5,totals,cell_style)
         
         #sheet.write(i, 5, bom.code or '')
         i += 1
         for product, total_qty in requirements.items():
-            sheet.write(i, 1, product.default_code or '',cell_style)
-            sheet.write(i, 2, product.display_name or '',cell_style)
-            sheet.write(i, 3, total_qty or 0.0,cell_style)
-            sheet.write(i, 4, product.uom_id.name or '',cell_style)
-            sheet.write(i, 5, product.product_tmpl_id.standard_price or 0.0,cell_style)
+            sheet.write(i, 0, product.default_code or '',cell_style)
+            sheet.write(i, 1, product.product_tmpl_id.name or '',cell_style)
+            sheet.write(i, 2, total_qty or 0.0,cell_style)
+            sheet.write(i, 3, product.uom_id.name or '',cell_style)
+            sheet.write(i, 4, product.product_tmpl_id.standard_price or 0.0,cell_style)
             totals = self.get_totals(product.product_tmpl_id.id,locations)
-            sheet.write_row(i,6,totals,cell_style)
+            sheet.write_row(i,5,totals,cell_style)
             i += 1
         return i
 
@@ -60,9 +61,9 @@ class FlattenedBomXlsx(models.AbstractModel):
             #sheet.set_column(0, i, 10)
 
         #sheet.set_column(0, 1, 17)
-        sheet.set_column(0, 0, 30)
-        sheet.set_column(1, 1, 18)
-        sheet.set_column(2, 2, 56)
+        sheet.set_column(0, 0, 17)
+        sheet.set_column(1, 1, 45)
+        sheet.set_column(2, 2, 8)
         sheet.set_column(3, 11, 13)
         
 
@@ -74,9 +75,13 @@ class FlattenedBomXlsx(models.AbstractModel):
                                                 'bg_color': '#FFC7CE',
                                                 'font_color': '#9C0006'})
         locations = self.env['stock.location'].search([('id', 'in', [13, 86, 18, 12, 21])])
+        user = self.env['res.users'].browse(self.env.uid)
+        tz = pytz.timezone(user.tz)
+        time = pytz.utc.localize(datetime.now()).astimezone(tz)
+        sheet.merge_range(0, 0, 0, 3, 'Reporte # 2: Requerimiento de Materiales por Numero de Parte', title_style)
+        sheet.merge_range('A4:C4', 'Report Date: ' + str(time.strftime("%Y-%m-%d %H:%M %p")), title_style)
 
-        sheet_title = [_('BOM Name'),
-                       _('Product Reference'),
+        sheet_title = [_('Product Reference'),
                        _('Product Name'),
                        _('Quantity'),
                        _('UM'),
@@ -92,10 +97,9 @@ class FlattenedBomXlsx(models.AbstractModel):
             sheet_title.append(_(name))
 
         sheet_title.append(_('Total'))
-        sheet.write(0, 0,"Reporte # 2: Requerimiento de Materiales por Numero de Parte",title_style)
-        sheet.write_row(1, 0, sheet_title, title_style)
-        sheet.freeze_panes(2, 0)
-        i = 2
+        #sheet.write(0, 0,"Reporte # 2: Requerimiento de Materiales por Numero de Parte",title_style)
+        sheet.write_row(5, 0, sheet_title, title_style)
+        i = 6
 
         for o in objects:
             # We need to calculate the totals for the BoM qty and UoM:
