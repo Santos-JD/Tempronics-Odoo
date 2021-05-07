c24ada41f0eba1739ea6d45ffefc0148ff549053
J-Monroy <jmonroy@tempronics.com>
2021-05-07 11:13:39 -0700

    [MOD] Send Email Inventory Adjustment

--- tempronics_report/__manifest__.py
+++ tempronics_report/__manifest__.py
@@ -21,7 +21,8 @@
         'wizard/wizard_temp_report_stock_location_view.xml',
         'wizard/wizard_temp_report_bom_location_view.xml',
         'wizard/wizard_temp_report_mps_view.xml',
-        'data/email_template_report_stock_month.xml'
+        'data/email_template_report_stock_month.xml',
+        'data/email_template_inventory_adjustment.xml',
     ],
     'application': True,
 }
